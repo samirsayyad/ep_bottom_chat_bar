8e5cdd4d04b872cd8bc8ab578de51a42ff981767
Samir Sayyad <samir.saiad@gmail.com>
2020-06-26 17:03:08 +0430

    god please help copy paster :D

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var eejs = require("ep_etherpad-lite/node/eejs");
 
 exports.eejsBlock_styles = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_chatdate/templates/styles.html", {}, module);
+  args.content = args.content + eejs.require("ep_bottom_chat_bar/templates/styles.html", {}, module);
   return cb();
 };
