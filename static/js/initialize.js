5f510aa03b270eed8b0020a55fce83887d4fe1f4
Samir Sayyad <samir.saiad@gmail.com>
2020-06-26 18:46:22 +0430

    how about resize ?

--- static/js/initialize.js
+++ static/js/initialize.js
@@ -1,6 +1,6 @@
 exports.aceInitialized = function(hook, context){
     //chat.stickToScreen(true);
     chat.show()
-    $( "#chatbox" ).resizable({ maxWidth: "100%" });
+    $("#chatbox").resizable({  maxWidth: "100%" ,handles: 'n, e, s, w' });
 
 }
