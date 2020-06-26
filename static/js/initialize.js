2fa6b1007f584814963ea501ad143109df228e80
Samir Sayyad <samir.saiad@gmail.com>
2020-06-26 20:27:54 +0430

    resize something and shadow

--- static/js/initialize.js
+++ static/js/initialize.js
@@ -1,6 +1,30 @@
 exports.aceInitialized = function(hook, context){
     //chat.stickToScreen(true);
     chat.show()
-    $("#chatbox").resizable({  maxWidth: "100%" ,handles: 'n, e, s, w' });
+
+
+    $('#chatbox').on('mousedown', function(e){
+        console.log("mousedown",e.pageX)
+        var $dragable = $(".chat-content") ,
+            startHeight = $dragable.height(),
+            pX = e.pageX;
+        
+        $(document).on('mouseup', function(e){
+            $(document).off('mouseup').off('mousemove');
+        });
+        $(document).on('mousemove', function(me){
+
+            var mx = (me.pageX - pX);
+
+            console.log("mousemove",mx)
+
+            $dragable.css({
+                height: startHeight + mx,
+                //top: my
+            });
+        });
+                
+    });
+
 
 }
