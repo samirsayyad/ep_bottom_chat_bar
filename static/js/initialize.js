ac21eee1fc44cfa5d37dc23d931d501fcd03336a
Samir Sayyad <samir.saiad@gmail.com>
2020-06-26 20:46:28 +0430

    resizeable sucks

--- static/js/initialize.js
+++ static/js/initialize.js
@@ -3,28 +3,28 @@ exports.aceInitialized = function(hook, context){
     chat.show()
 
 
-    $('#chatbox').on('mousedown', function(e){
-        console.log("mousedown",e.pageX)
-        var $dragable = $(".chat-content") ,
-            startHeight = $dragable.height(),
-            pX = e.pageX;
+    // $('#chatbox').on('mousedown', function(e){
+    //     console.log("mousedown",e.pageX)
+    //     var $dragable = $(".chat-content") ,
+    //         startHeight = $dragable.height(),
+    //         pX = e.pageX;
         
-        $(document).on('mouseup', function(e){
-            $(document).off('mouseup').off('mousemove');
-        });
-        $(document).on('mousemove', function(me){
+    //     $(document).on('mouseup', function(e){
+    //         $(document).off('mouseup').off('mousemove');
+    //     });
+    //     $(document).on('mousemove', function(me){
 
-            var mx = (me.pageX - pX);
+    //         var mx = (me.pageX - pX);
 
-            console.log("mousemove",mx)
+    //         console.log("mousemove",mx)
 
-            $dragable.css({
-                height: startHeight + mx,
-                //top: my
-            });
-        });
+    //         $dragable.css({
+    //             height: startHeight + mx,
+    //             //top: my
+    //         });
+    //     });
                 
-    });
+    // });
 
 
 }
