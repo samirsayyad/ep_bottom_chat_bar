952aae8ef176fc5e0f5ed95fe773ae1b49098771
Samir Sayyad <samir.saiad@gmail.com>
2020-06-29 18:57:31 +0430

    resize box chat

--- static/js/initialize.js
+++ static/js/initialize.js
@@ -1,30 +1,141 @@
 exports.aceInitialized = function(hook, context){
     //chat.stickToScreen(true);
+    var isDragging = false;
+    var isMouseDown = false
+    var padOuter = null;
+	var padInner = null;
+	var outerBody = null;
+    var innerBody = null ;
+    var startHeight , pY ;
+    var docHeight =$(document).height() ;
+
+
     chat.show()
+    padOuter = $('iframe[name="ace_outer"]').contents();
+    padInner = padOuter.find('iframe[name="ace_inner"]').contents();
+    outerBody = padOuter.find("#outerdocbody");
+    innerBody = padInner.find("#innerdocbody");
+    /**
+     * resizer inserting
+     */
+    $(".chat-content").attr('id', 'chatBoxBottom');
+    var chatBoxBottom = document.getElementById("chatBoxBottom")
+    var resizer = document.createElement('div');
+    resizer.className = 'resizer';
+    resizer.id = 'resizer';
+    chatBoxBottom.prepend(resizer);
 
+    /**
+     * Mouse down
+     */
 
-    // $('#chatbox').on('mousedown', function(e){
-    //     console.log("mousedown",e.pageX)
-    //     var $dragable = $(".chat-content") ,
-    //         startHeight = $dragable.height(),
-    //         pX = e.pageX;
+    $("#resizer").on('mousedown', function(event){
+        startHeight = parseInt(document.defaultView.getComputedStyle(chatBoxBottom).height, 10);
+        pY = event.pageY;
+        isDragging = false;
+        isMouseDown = true;
+        $("#resizer").css({"background-color":"red"})
+        $("body").append("<div id='ghost_chat_resizer' style='top : "+event.pageY+"px'></div>")
+        e=event || window.event;
+        pauseEvent(e);
         
-    //     $(document).on('mouseup', function(e){
-    //         $(document).off('mouseup').off('mousemove');
-    //     });
-    //     $(document).on('mousemove', function(me){
+    })
+    /**
+     * mouse move
+     */
+    $(document).on('mousemove', function(event){
+        console.log("mousemove ",event.pageY)
 
-    //         var mx = (me.pageX - pX);
+        isDragging= true
+        if(isMouseDown){
+            $("#resizer").css({"background-color":"brown"})
+            $('#ghost_chat_resizer').css({
+                top:   event.pageY
+            });
+        }else{
+            $("#ghost_chat_resizer").remove()
+        }
 
-    //         console.log("mousemove",mx)
+    });
 
-    //         $dragable.css({
-    //             height: startHeight + mx,
-    //             //top: my
-    //         });
-    //     });
-                
-    // });
+    outerBody.on("mousemove",function(event){
+        console.log("mousemove ",event.pageY)
+        isDragging= true
+        if(isMouseDown){
+            $("#resizer").css({"background-color":"brown"})
+            $('#ghost_chat_resizer').css({
+                top:   event.pageY +85
+            });
+        }else{
+            $("#ghost_chat_resizer").remove()
+        }
+    })
+    innerBody.on("mousemove",function(event){
+        console.log("mousemove ",event.pageY)
+        isDragging= true
+        if(isMouseDown){
+            $("#resizer").css({"background-color":"brown"})
+            $('#ghost_chat_resizer').css({
+                top:   event.pageY + 145
+            });
+        }else{
+            $("#ghost_chat_resizer").remove()
+        }
+    })
 
+
+    /**
+     * mouseup
+     */
+    $(document).on('mouseup', function(event){
+        console.log("mouseup document")
+        $("#resizer").css({"background-color":"green"})
+        $("#ghost_chat_resizer").remove()
+
+        isDragging = false;
+        isMouseDown = false ;
+        resizeChatBar(event.pageY)
+
+    });
+
+
+    outerBody.on("mouseup",function(event){
+        console.log("mouseup outerBody")
+        $("#resizer").css({"background-color":"green"})
+        $("#ghost_chat_resizer").remove()
+
+        isDragging = false;
+        isMouseDown = false ;
+        resizeChatBar(event.pageY+85)
+
+    })
+    innerBody.on("mouseup",function(event){
+        console.log("mouseup innerBody ")
+        $("#resizer").css({"background-color":"green"})
+        $("#ghost_chat_resizer").remove()
+
+        isDragging = false;
+        isMouseDown = false ;
+        resizeChatBar(event.pageY+ 145)
+
+    })
+    function pauseEvent(e){
+        if(e.stopPropagation) e.stopPropagation();
+        if(e.preventDefault) e.preventDefault();
+        e.cancelBubble=true;
+        e.returnValue=false;
+        return false;
+    }
+    function resizeChatBar(pageY){
+        var chatBoxBottom = $("#chatBoxBottom")
+        
+        var mY = (pY - pageY );
+        //console.log(mY  ,   pageY , pY , startHeight , (startHeight - mY))
+        var newHeight = (startHeight + mY < 10) ?  10 : startHeight + mY ;
+        var newHeight = (newHeight >= docHeight ) ?  docHeight : newHeight;
+        chatBoxBottom.css({
+            height: newHeight,
+        });
+    }
 
 }
